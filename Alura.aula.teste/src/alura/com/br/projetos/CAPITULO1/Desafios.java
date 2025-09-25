5f9ad7c38cffd11862af610d8131b5d05b7c67d3
Kaue pires <161778362+okauezo@users.noreply.github.com>
2025-09-24 22:24:26 -0300

    Add files via upload

--- Alura.aula.teste/src/alura/com/br/projetos/CAPITULO1/Desafios.java
+++ Alura.aula.teste/src/alura/com/br/projetos/CAPITULO1/Desafios.java
@@ -27,5 +27,19 @@ public class Desafios {
 
         String mensagem = "" + letra + letra2  + " " + palavra;
         System.out.println(mensagem);
+
+        // 4 Declare uma variável do tipo double precoProduto e uma variável do tipo int (quantidade).
+        // Calcule o valor total multiplicando o preço do produto pela quantidade e apresente o resultado em uma mensagem.
+
+        double precoProduto = 20.99;
+        int quantidade = 4;
+        double total = (precoProduto * quantidade);
+        System.out.println("preco produto: " + total);
+
+
+        // 5 Declare uma variável do tipo double valorEmDolares. Atribua um valor em dólares a essa variável. Considere que o valor de 1 dólar é equivalente a 4.94 reais.
+        // Realize a conversão do valor em dólares para reais e imprima o resultado formatado.
+
+        
     }
 }
